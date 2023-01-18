dcb111bd9a8a6580ecc544e4916fcd6061b82f8d
doomsday26 <harshnamdeo261999@gmail.com>
2023-01-18 12:15:46 +0530

    types

--- type.js
+++ type.js
@@ -1,4 +1,20 @@
-var add = function (num1, num2) {
-    console.log(num1 + num2);
-};
-add(2, 3);
+var num1 = document.getElementById('num1');
+var number1 = num1.value;
+var num2 = document.getElementById('num2');
+var number2 = num1.value;
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function () {
+    var a = add(number1, number2);
+    console.log(a);
+});
+function add(num1, num2) {
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    }
+    if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return (+num1 + +num2);
+    }
+    else {
+        return "enter proper type";
+    }
+}
--- type.ts
+++ type.ts
@@ -1,5 +1,26 @@
-var add=(num1:number,num2:number)=>{
-    console.log(num1+num2);
+let num1= document.getElementById('num1') as HTMLInputElement
+let number1= num1.value
+
+let num2= document.getElementById('num2') as HTMLInputElement
+let number2= num1.value
+
+let btn =document.getElementById('btn') !
+btn.addEventListener('click',()=>{
+let a=add(number1,number2)
+console.log(a);
+})
+
+
+
+function add(num1:number|string,num2:number|string){
+    if(typeof num1==='number'&&typeof num2==='number'){
+        return num1+ num2
+    }
+    if(typeof num1==='string'&&typeof num2==='string'){
+        return(+num1 + +num2)
+    }else{
+        return "enter proper type"
+    }
 }
 
-add(2,3);
+
