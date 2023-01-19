1af8550148ec4ddb0f68b191d7bc5f60387f129a
doomsday26 <harshnamdeo261999@gmail.com>
2023-01-19 14:02:48 +0530

    toto rest api

---
+++ app.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import todoRoutes from './routes/todos';
+import bodyParser from 'body-parser';
+const app= express()
+app.use(bodyParser.json())
+app.use(todoRoutes)
+
+app.listen(3000,()=>{
+    console.log("running on port 3000");
+});
